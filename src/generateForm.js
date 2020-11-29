1ab07525194e8b6bca9d1d93190d31b125ed28f9
ArianPaymozd <arianyazdi@yahoo.com>
2020-11-29 13:48:31 -0800

    src folder added

---
+++ src/generateForm.js
@@ -0,0 +1,36 @@
+let generateForm = () => {
+    return "generate form";
+}
+
+let generateTextField = (label, id, placeholder) => {
+    let textField =``
+    if (placeholder) {
+        textField = `<input type="text" id="${id}" name="${id}" placeholder="${placeholder}"required>`;
+    }else {
+        textField = `<input type="text" id="${id}" name="${id}" required>`;
+    }
+   return `
+   <label for="${id}">${label} </label>
+   ${textField}
+   `
+}
+
+let generateNumberOption = (label, id) => {
+    let numberOption = `<input type="number" id="${id}" name="${id}" min="1" max="5" required>`;
+   return `
+   <label for="${id}">${label} </label>
+   ${numberOption}
+   `
+}
+
+let generateButton = (text, id, uid) => {
+   let button = `<button type="submit" class="${id}" id="${id}${uid}" name="${id}" data-id="${uid}"> ${text} </button>`;
+   return button;
+}
+
+export default {
+    generateForm,
+    generateTextField,
+    generateNumberOption,
+    generateButton
+}
